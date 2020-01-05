f31fd57518d5993687c23cedb7ac19ac54d3ccc1
win11905 <donothavemoney@gmail.com>
2020-01-05 23:52:14 +0700

    debug

--- gengenpdf.py
+++ gengenpdf.py
@@ -1,5 +1,5 @@
 for i in range(12):
 	s = input()
 	print('cd ./{}'.format(s))
-	print('/Library/TeX/texbin/xelatex -interaction nonstopmode -halt-on-error -file-line-error problem.tex')
+	print('/Library/TeX/texbin/xelatex -interaction nonstopmode  problem.tex')
 	print('cd ..')
